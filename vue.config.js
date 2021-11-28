9fca6198cf4730f3188c1fa63d3390b8c203dff2
Vadim Saprykin <sprkweb@yandex.ru>
2021-11-28 18:05:44 +0300

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -6,21 +6,21 @@ module.exports = {
       background_color: '#FFFFFF',
       icons: [
         {
-          "src": "./img/icons/icon.svg",
-          "type": "image/svg+xml",
-          "purpose": "any maskable"
+          src: './img/icons/icon.svg',
+          type: 'image/svg+xml',
+          purpose: 'any maskable'
         },
         {
-          "src": "./img/icons/icon-192.png",
-          "type": "image/png",
-          "sizes": "192x192",
-          "purpose": "any maskable"
+          src: './img/icons/icon-192.png',
+          type: 'image/png',
+          sizes: '192x192',
+          purpose: 'any maskable'
         },
         {
-          "src": "./img/icons/icon-512.png",
-          "type": "image/png",
-          "sizes": "512x512",
-          "purpose": "any maskable"
+          src: './img/icons/icon-512.png',
+          type: 'image/png',
+          sizes: '512x512',
+          purpose: 'any maskable'
         }
       ]
     },
@@ -35,8 +35,19 @@ module.exports = {
     }
   },
 
-  configureWebpack: {
-    devtool: 'source-map'
+  // source map
+  // thanks @kkxujq https://github.com/vuejs/vue-cli/issues/2978#issuecomment-441426094
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'development') {
+      config.devtool = 'eval-source-map'
+
+      config.output.devtoolModuleFilenameTemplate = info =>
+        info.resourcePath.match(/^\.\/\S*?\.vue$/) && !info.identifier.match(/type=script/)
+          ? `webpack-generated:///${info.resourcePath}?${info.hash}`
+          : `webpack-yourCode:///${info.resourcePath}`
+
+      config.output.devtoolFallbackModuleFilenameTemplate = 'webpack:///[resource-path]?[hash]'
+    }
   },
 
   transpileDependencies: [
