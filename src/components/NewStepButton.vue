e13c8de502bba617eb55be6a403c8913499a1f42
Vadim Saprykin <sprkweb@yandex.ru>
2021-12-12 20:51:07 +0300

    dialog for adding a new step

--- src/components/NewStepButton.vue
+++ src/components/NewStepButton.vue
@@ -1,36 +1,70 @@
 <template>
-  <v-speed-dial
-      v-model="speedDialShown"
-      fixed bottom right
-      transition="slide-y-reverse-transition"
-      direction="top"
-    >
-      <template v-slot:activator>
-        <v-btn
-          v-model="speedDialShown"
-          fab
-          color="accent"
+  <v-dialog
+    v-model="dialogShown"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        fab
+        color="accent"
+        fixed bottom right
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>
+          {{ icons.mdiPlus }}
+        </v-icon>
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title>
+        {{ $t('chooseStepDialog.title') }}
+      </v-card-title>
+      <v-card-text>
+        <v-radio-group
+          v-model="selectedModule"
+          column
         >
-          <v-icon v-if="speedDialShown">
-            {{ icons.mdiClose }}
-          </v-icon>
-          <v-icon v-else>
-            {{ icons.mdiPlus }}
-          </v-icon>
+          <v-radio
+            v-for="(stepModule) in stepModules"
+            :key="stepModule.name"
+            :value="stepModule.name"
+            :label="$t(`stepModules.${stepModule.name}.name`)"
+          ></v-radio>
+        </v-radio-group>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          text
+          color="error"
+          @click="dialogShown = false"
+        >
+          {{ $t('button.cancel') }}
         </v-btn>
-      </template>
-      <p>Hey</p>
-    </v-speed-dial>
+        <v-btn
+          text
+          color="primary"
+          :disabled="selectedModule === null"
+          @click="dialogShown = false"
+        >
+          {{ $t('button.add') }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { mdiClose, mdiPlus } from '@mdi/js'
+import { index as stepModules } from '@/model/step-modules'
 
 export default Vue.extend({
   data: () => ({
-    speedDialShown: false,
-    icons: { mdiClose, mdiPlus }
+    icons: { mdiClose, mdiPlus },
+    dialogShown: false,
+    selectedModule: null,
+    stepModules
   })
 })
 </script>
--- src/model/step-modules/index.ts
+++ src/model/step-modules/index.ts
@@ -1,3 +1,14 @@
 export { default as Initialize } from './Initialize'
 export { default as Contribution } from './Contribution'
 export { default as Income } from './Income'
+
+export const index = [
+  {
+    name: 'contribution',
+    module: 'Contribution'
+  },
+  {
+    name: 'income',
+    module: 'Income'
+  }
+]
