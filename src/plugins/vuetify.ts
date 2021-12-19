ad7d17c43d8a65b172218261e20cd12cb68a3baf
Vadim Saprykin <sprkweb@yandex.ru>
2021-12-19 13:21:54 +0300

    remove vuetify integration with vue-i18n

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import Vuetify, { VExpansionPanels } from 'vuetify/lib'
 
-import VueI18n from 'vue-i18n'
-import i18n from '../i18n'
-
 import '../styles/styles.scss'
 
 Vue.use(Vuetify, {
@@ -11,12 +8,8 @@ Vue.use(Vuetify, {
     VExpansionPanels
   }
 })
-Vue.use(VueI18n)
 
 export default new Vuetify({
-  lang: {
-    t: (key, ...params) => i18n.t(key, params) as string
-  },
   theme: {
     themes: {
       light: {
